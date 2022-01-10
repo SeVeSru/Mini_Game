11a70a434b6bee90cbae400ba9e2b4350c368153
SeVeSru <qwertysp96@gmail.com>
2022-01-10 13:51:26 +0700

    Реализация изменения размера поля

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,22 +6,24 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            int[,] Field = new int[25, 25];
+            int SizeXY = 25;
+
+            int[,] Field = new int[SizeXY, SizeXY];
             Random random = new Random();
 
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < SizeXY; i++)
             {
-                for (int j = 0; j < 25; j++)
+                for (int j = 0; j < SizeXY; j++)
                 {
                     Field[i, j] = 0;
                 }
             }
 
-            string player1Str = "OO";
-            string player2Str = "XX";
-            string fieldStr = "##";
+            string player1Str = " O";
+            string player2Str = " X";
+            string fieldStr = " #";
 
-            FieldOut(Field, player1Str, player2Str, fieldStr);
+            FieldOut(Field, player1Str, player2Str, fieldStr, SizeXY);
             Console.ReadKey();
 
             int round = 2;
@@ -32,19 +34,19 @@ namespace Game
                 else round = 1;
 
                 int x = random.Next(1, 7), y = random.Next(1, 7);
-                InterfaceMove(round, x, y, Field, player1Str, player2Str, fieldStr);
+                InterfaceMove(round, x, y, Field, player1Str, player2Str, fieldStr, SizeXY);
                 Console.ReadKey();
 
             } while (true);
         }
 
-        static void InterfaceMove(int move, int x, int y, int[,] F, string p1, string p2, string f)
+        static void InterfaceMove(int move, int x, int y, int[,] F, string p1, string p2, string f, int SizeXY)
         {
             int x1, y1;
             string sx1, sy1;
             Console.Clear();
 
-            FieldOut(F, p1, p2, f);
+            FieldOut(F, p1, p2, f, SizeXY);
             Console.WriteLine("Dice drop: " + x + ", and " + y);
             do
             {
@@ -72,37 +74,37 @@ namespace Game
                 {
                     for (int j = y1; j < y1 + y; j++)
                     {
-                        F[24 - i, 24 - j] = move;
+                        F[(SizeXY-1) - i, (SizeXY-1) - j] = move;
                     }
                 }
         }
 
-        static void FieldOut(int[,] F, string p1, string p2, string f)
+        static void FieldOut(int[,] F, string p1, string p2, string f, int SizeXY)
         {
-            for (int i = 0; i < 27; i++)
+            for (int i = 0; i < SizeXY+1; i++)
             {
-                for (int j = 0; j < 27; j++)
+                for (int j = 0; j < SizeXY+1; j++)
                 {
                     if (i == 0 && j == 0)
                         Console.Write("   ");
-                    else if (i == 0 && j < 11)
+                    else if (i == 0 && j < (SizeXY/2-1))
                         Console.Write(j - 1 + " ");
-                    else if (i == 0 && j == 26)
+                    else if (i == 0 && j == SizeXY + 1)
                         Console.Write("");
-                    else if (i < 11 && j == 0)
+                    else if (i < (SizeXY / 2 - 1) && j == 0)
                         Console.Write(" " + (i - 1) + " ");
-                    else if (i < 11 && j == 26)
-                        Console.Write(" " + (25 - i));
-                    else if (i == 26 && j == 26)
+                    else if (i < (SizeXY / 2 - 1) && j == SizeXY + 1)
+                        Console.Write(" " + (SizeXY - i));
+                    else if (i == SizeXY + 1 && j == SizeXY + 1)
                         Console.Write("  ");
-                    else if (j == 26)
-                        Console.Write(" " + (25 - i));
-                    else if (i == 26 && j == 0)
+                    else if (j == SizeXY + 1)
+                        Console.Write(" " + (SizeXY - i));
+                    else if (i == SizeXY + 1 && j == 0)
                         Console.Write("   ");
-                    else if (i == 26 && (25 - j) > 9)
-                        Console.Write(25 - j);
-                    else if (i == 26 && j != 0)
-                        Console.Write(" " + (25 - j));
+                    else if (i == SizeXY + 1 && (SizeXY - j) > 9)
+                        Console.Write(SizeXY - j);
+                    else if (i == SizeXY + 1 && j != 0)
+                        Console.Write(" " + (SizeXY - j));
                     else if (i == 0)
                         Console.Write(j - 1);
                     else if (j == 0)
